5a4ac9ac04da481175db7f6f15a240d9612f9c41
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-17 13:31:24 +0200

    add page data

---
+++ src/pageData.js
@@ -0,0 +1,19 @@
+export const pageData = {
+  header: {
+    nadpis: 'Jogíni',
+    polozkyMenu: [
+      { text: 'domů', url: '#' },
+      { text: 'lekce', url: '#' },
+      { text: 'náš tým', url: '#' },
+      { text: 'události', url: '#' },
+      { text: 'kontakt', url: '#' },
+    ],
+  },
+  intro: {
+    nadpis: 'Vítejte mezi Jogíny',
+    odstavec: `Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem. Postupně se seznámíte se základními principy jógy, jak přístupovat k sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu svoje tělo, pocity a emoce.`,
+  },
+  pose: {
+    cestaKObrazku: 'img/yoga-pose.jpg',
+  },
+};
